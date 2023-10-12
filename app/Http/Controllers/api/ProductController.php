2a06c42387673645bc80ccf7f2bf98d569e5d75e
shadw77 <Shady2018170763@cis.asu.edu.eg>
2023-10-12 13:55:41 +0300

    validate api request

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -3,18 +3,77 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Response;
+use Illuminate\Validation\Rule;
 
 class ProductController extends Controller
 {
-    //
-    function index(){
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
         $products = Product::all();
         return $products;
     }
 
-    function show(Product $product){
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        // dd($request);
+        $validator = Validator::make($request->all(),[
+            'name'=>'unique:products'
+        ]);
+        if($validator->fails()){
+            return response($validator->errors()->all(), 422);
+        }
+       
+        $product = Product::create($request->all());
+        return response($product,201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
         return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+        $validator = Validator::make($request->all(),[
+            'name'=>[ Rule::unique('products')->ignore($product->name),]
+        ]);
+        if($validator->fails()){
+            return response($validator->errors()->all(), 422);
+        }
+       
+        $product->update($request->all());
+        return response($product,200);
+        
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+        $product->delete();
+        return 'deleted';
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // API
 use App\Http\Controllers\api\ProductController;
-Route::get('products',[ProductController::class,'index']);
-Route::get('products/{product}',[ProductController::class,'show']);
+// Route::get('products',[ProductController::class,'index']);
+// Route::get('products/{product}',[ProductController::class,'show']);
+Route::apiResource('products', ProductController::class);
 
